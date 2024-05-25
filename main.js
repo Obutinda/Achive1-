9f3d9abce58932fee586f73cb5b666b7cd07f4ed
Obutinda <161114147+Obutinda@users.noreply.github.com>
2024-05-25 12:19:33 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,62 @@
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+// JavaScript
+/* Function to toggle the dropdown */
+function toggleDropdown() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+//---read more button--//
+$('#moreContent').on('shown.bs.collapse', function () {
+  $('[href="#moreContent"]').text('Read Less');
+});
+
+$('#moreContent').on('hidden.bs.collapse', function () {
+  $('[href="#moreContent"]').text('Read More');
+});
+header=document.getElementById("header");
+        header.textContent="IYF E-LEARNING PORTAL";
+        function alertMessage(){
+            alert("Welcome to Obutinda's Website. To view and learn all the courses provided please go to contacts in the navigation bar");}
+            window.onload=alertMessage;
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
